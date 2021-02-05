dad013c2d7453416d1d4b165f77e61cf4a3e4454
Cort Spellman <spellman.cort@gmail.com>
2021-02-05 00:42:53 -0600

    Add health_check endpoint that returns empty 200

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web;
+use actix_web::web;
+
+// async fn greet(req: actix_web::HttpRequest) -> impl actix_web::Responder {
+//     let name = req.match_info().get("name").unwrap_or("World");
+//     format!("Hello {}!", &name)
+// }
+
+async fn health_check() -> impl actix_web::Responder {
+    actix_web::HttpResponse::Ok()
+}
+
+#[actix_rt::main]
+async fn main() -> std::io::Result<()> {
+    actix_web::HttpServer::new(|| {
+        actix_web::App::new()
+            .route("/health_check", web::get().to(health_check))
+    })
+        .bind("127.0.0.1:8000")?
+        .run()
+        .await
 }
